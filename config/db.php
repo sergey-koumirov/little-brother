91722c2c40654d5b2f9c9aa6590c04f85b552db1
sergey.koumirov <sergey.koumirov@gmail.com>
2016-04-02 11:38:42 +0400

    initialized yii2 (this time with files!)

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=little_brother_dev',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
